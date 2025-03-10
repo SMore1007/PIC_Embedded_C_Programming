01273110f3376bb84e320c617b51f45b17b97bff
Sagar More <sagarmore1007@gmail.com>
2025-03-10 16:48:56 +0530

    main file

--- Class Projects/Class-3 LCD Interface/Class_3_Class_Program.X/main.c
+++ Class Projects/Class-3 LCD Interface/Class_3_Class_Program.X/main.c
@@ -17,61 +17,68 @@
 #pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 
-#define _XTAL_FREQ 20000000
 
+#define _XTAL_FREQ 20000000  // Define the crystal oscillator frequency as 20MHz (for delay functions)
 
-void  Lcdinit(void);
-void  LcdCommand(uint8_t);
-void  LcdData(uint8_t);
+void Lcdinit(void);          // Function prototype for LCD initialization
+void LcdCommand(uint8_t);    // Function prototype for sending commands to the LCD
+void LcdData(uint8_t);       // Function prototype for sending data to the LCD
 
-uint8_t x, i, m;
+uint8_t x, i, m;             // Declare global variables
 
-uint8_t a[18] = {"HELLO WORLD"};
+uint8_t a[18] = {"HELLO WORLD"}; // Character array storing the message to display
 
 void main(void) {
-    Lcdinit();
-    for(int i = 0; i < 11; i++)
-    {
-        LcdCommand(0x80 + i);  // Increment from 0x80 
-        LcdData(a[i]);
+    Lcdinit();  // Initialize the LCD
+
+    // Loop to display "HELLO WORLD" on the LCD
+    for (int i = 0; i < 11; i++) {  
+        LcdCommand(0x80 + i);  // Move the cursor to the respective position on the LCD (starting from 0x80)
+        LcdData(a[i]);         // Send each character to the LCD
     }
-    while(1); // loop forever
+    
+    while(1);  // Infinite loop to keep the program running
 }
 
-void  Lcdinit(void)
-{
-    TRISC = 0x00;
-    TRISD = 0x00;
+// Function to initialize the LCD
+void Lcdinit(void) {
+    TRISC = 0x00;  // Set PORTC as output (for control signals)
+    TRISD = 0x00;  // Set PORTD as output (for data signals)
+    
+    __delay_ms(100);  // Wait for LCD to stabilize
+    
+    // LCD initialization sequence as per the HD44780 LCD datasheet
+    LcdCommand(0x30);  // Send function set command (8-bit mode)
+    __delay_ms(100);   // Delay for command execution
+    LcdCommand(0x30);  // Repeat function set command
     __delay_ms(100);
-    LcdCommand(0x30);
+    LcdCommand(0x30);  // Repeat function set command again
     __delay_ms(100);
-    LcdCommand(0x30);
+    
+    LcdCommand(0x38);  // Set LCD for 8-bit mode, 2-line display, 5x8 font
     __delay_ms(100);
-    LcdCommand(0x30);
+    
+    LcdCommand(0x0C);  // Turn on display, cursor off
     __delay_ms(100);
-    LcdCommand(0x38);
-    __delay_ms(100);
-    LcdCommand(0x0C); // Display on Cursor Off
-    __delay_ms(100);
-    LcdCommand(0x01); // Clear Display
+    
+    LcdCommand(0x01);  // Clear the display
     __delay_ms(100);
 }
 
-
-void  LcdData(uint8_t i)
-{
-    PORTC |= (0x1 << 3);  // RS = 1; bitwise Operation
-    PORTD = i;
-    PORTC |= (0x1 << 0);  // EN = 1; bitwise Operation
-    __delay_ms(100);
-    PORTC &= ~(0x1 << 0);  // EN = 0; bitwise Operation
+// Function to send data (characters) to the LCD
+void LcdData(uint8_t i) {
+    PORTC |= (0x1 << 3);  // Set RS (RC3) = 1 (indicates data mode)
+    PORTD = i;            // Place data on PORTD
+    PORTC |= (0x1 << 0);  // Set EN (RC0) = 1 (enable pulse start)
+    __delay_ms(100);      // Small delay for command execution
+    PORTC &= ~(0x1 << 0); // Set EN (RC0) = 0 (enable pulse end)
 }
 
-void  LcdCommand(uint8_t i)
-{
-    PORTC &= ~(0x1 << 3);   // RC3 = 0; bitwise Operation
-    PORTD  = i;
-    PORTC |= (0x1 << 0);    //    RC0 = 1;  bitwise Operation
-    __delay_ms(100);
-    PORTC &= ~(0x1 << 0);   //    RC0 = 0;  bitwise Operation
+// Function to send commands to the LCD
+void LcdCommand(uint8_t i) {
+    PORTC &= ~(0x1 << 3);  // Set RS (RC3) = 0 (indicates command mode)
+    PORTD = i;             // Place command on PORTD
+    PORTC |= (0x1 << 0);   // Set EN (RC0) = 1 (enable pulse start)
+    __delay_ms(100);       // Small delay for command execution
+    PORTC &= ~(0x1 << 0);  // Set EN (RC0) = 0 (enable pulse end)
 }
--- Task Projects/Class-3, Task LCD Interface/Class_3_Task_Program.X/main.c
+++ Task Projects/Class-3, Task LCD Interface/Class_3_Task_Program.X/main.c
@@ -19,68 +19,79 @@
 #pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
 
 
-#define _XTAL_FREQ 20000000
+#define _XTAL_FREQ 20000000  // Define the crystal oscillator frequency as 20MHz (for delay functions)
 
+void Lcdinit(void);          // Function prototype for LCD initialization
+void LcdCommand(uint8_t);    // Function prototype for sending commands to the LCD
+void LcdData(uint8_t);       // Function prototype for sending data to the LCD
 
-void  Lcdinit(void);
-void  LcdCommand(uint8_t);
-void  LcdData(uint8_t);
+uint8_t x, i, m;             // Declare global variables
 
-uint8_t x, i, m;
-
-uint8_t name[18] = {"SAGAR"};
+uint8_t name[18] = {"SAGAR"}; // Character array storing the name to display
 
 void main(void) 
 {
-    Lcdinit();
-    for (uint8_t i = 0; i < 11; i++)
+    Lcdinit();  // Initialize the LCD
+    
+    // Display "SAGAR" starting from the first row, first column (0x80)
+    for (uint8_t i = 0; i < 11; i++)  
     {
-        LcdCommand((uint8_t)(0x80 + i));  // type-casting
-        LcdData(name[i]);
+        LcdCommand((uint8_t)(0x80 + i));  // Move the cursor to the respective position (casting to uint8_t)
+        LcdData(name[i]);                 // Send each character to the LCD
     }
-    for (uint8_t i = 0; i < 11; i++) 
+
+    // Display "SAGAR" starting from the second row, last column (0xCF)
+    for (uint8_t i = 0; i < 11; i++)  
     {
-        LcdCommand((uint8_t)(0xCF - i));  // type-casting
-        LcdData(name[i]);
+        LcdCommand((uint8_t)(0xCF - i));  // Move cursor from right to left on the second row (casting to uint8_t)
+        LcdData(name[i]);                 // Send each character to the LCD
     }
     
-    while(1); // loop forever
+    while(1);  // Infinite loop to keep the program running
 }
 
-void  Lcdinit(void)
+// Function to initialize the LCD
+void Lcdinit(void)
 {
-    TRISC = 0x00;
-    TRISD = 0x00;
+    TRISC = 0x00;  // Set PORTC as output (for control signals)
+    TRISD = 0x00;  // Set PORTD as output (for data signals)
+
+    __delay_ms(100);  // Wait for LCD to stabilize
+    
+    // LCD initialization sequence as per HD44780 LCD datasheet
+    LcdCommand(0x30);  // Send function set command (8-bit mode)
+    __delay_ms(100);   
+    LcdCommand(0x30);  // Repeat function set command
     __delay_ms(100);
-    LcdCommand(0x30);
+    LcdCommand(0x30);  // Repeat function set command again
     __delay_ms(100);
-    LcdCommand(0x30);
+    
+    LcdCommand(0x38);  // Set LCD for 8-bit mode, 2-line display, 5x8 font
     __delay_ms(100);
-    LcdCommand(0x30);
+    
+    LcdCommand(0x0C);  // Turn on display, cursor off
     __delay_ms(100);
-    LcdCommand(0x38);
-    __delay_ms(100);
-    LcdCommand(0x0C); // Display on Cursor Off
-    __delay_ms(100);
-    LcdCommand(0x01); // Clear Display
+    
+    LcdCommand(0x01);  // Clear the display
     __delay_ms(100);
 }
 
-
-void  LcdData(uint8_t i)
+// Function to send data (characters) to the LCD
+void LcdData(uint8_t i)
 {
-    PORTC |= (0x1 << 3);  // RS = 1; bitwise Operation
-    PORTD = i;
-    PORTC |= (0x1 << 0);  // EN = 1; bitwise Operation
-    __delay_ms(100);
-    PORTC &= ~(0x1 << 0);  // EN = 0; bitwise Operation
+    PORTC |= (0x1 << 3);  // Set RS (RC3) = 1 (indicates data mode)
+    PORTD = i;            // Place data on PORTD
+    PORTC |= (0x1 << 0);  // Set EN (RC0) = 1 (enable pulse start)
+    __delay_ms(100);      // Small delay for command execution
+    PORTC &= ~(0x1 << 0); // Set EN (RC0) = 0 (enable pulse end)
 }
 
-void  LcdCommand(uint8_t i)
+// Function to send commands to the LCD
+void LcdCommand(uint8_t i)
 {
-    PORTC &= ~(0x1 << 3);   // RC3 = 0; bitwise Operation
-    PORTD  = i;
-    PORTC |= (0x1 << 0);    //    RC0 = 1;  bitwise Operation
-    __delay_ms(100);
-    PORTC &= ~(0x1 << 0);   //    RC0 = 0;  bitwise Operation
+    PORTC &= ~(0x1 << 3);  // Set RS (RC3) = 0 (indicates command mode)
+    PORTD = i;             // Place command on PORTD
+    PORTC |= (0x1 << 0);   // Set EN (RC0) = 1 (enable pulse start)
+    __delay_ms(100);       // Small delay for command execution
+    PORTC &= ~(0x1 << 0);  // Set EN (RC0) = 0 (enable pulse end)
 }
